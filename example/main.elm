574f98de2135cf264b74ddd1bc69d6ceae8b6d2d
Shinichiro Suzuki <shinichiro.su@gmail.com>
2015-04-20 15:06:38 +0900

    lineやbar関数に渡す引数をjson文字列じゃなくてに変更した

--- example/main.elm
+++ example/main.elm
@@ -5,51 +5,54 @@ import Debug
 
 chrt = Chart.chart("canvas")
 
-data = encode 0 <| object [
-      ("labels", list [(string "January"),(string "February"),(string "March"),(string "April"),(string "May"),(string "June"),(string "July")]),
-      ("datasets", list [ object [
-         ("fillColor", string "rgba(220,220,220,0.5)"),
-         ("strokeColor", string "rgba(220,220,220,0.8)"),
-         ("highlightFill", string "rgba(220,220,220,0.75)"),
-         ("highlightStroke", string "rgba(220,220,220,1)"),
-         ("data", list [int 30, int 34, int  67, int 12, int 96, int 75, int 39])
-       ],
-       object [
-         ("fillColor", string "rgba(151,187,205,0.5)"),
-         ("strokeColor", string "rgba(151,187,205,0.8)"),
-         ("highlightFill", string "rgba(151,187,205,0.75)"),
-         ("highlightStroke", string "rgba(151,187,205,1)"),
-         ("data", list [int 20, int 4, int 87,int 32, int 46, int 55,int 38])
-       ]]
-      )
-    ]
-
 options = encode 0 <| object [
-             ("bezierCurve", bool False)
+             ("bezierCurve", bool True)
            , ("barShowStroke", bool False)
           ]
--- data = {
---       labels = ["January","February","March","April","May","June","July"],
---       datasets = [
---        {
---          fillColor = "rgba(220,220,220,0.5)",
---          strokeColor = "rgba(220,220,220,0.8)",
---          highlightFill = "rgba(220,220,220,0.75)",
---          highlightStroke = "rgba(220,220,220,1)",
---          data = [30, 34, 67, 12, 96, 75, 39]
---        },
---        {
---          fillColor = "rgba(151,187,205,0.5)",
---          strokeColor = "rgba(151,187,205,0.8)",
---          highlightFill = "rgba(151,187,205,0.75)",
---          highlightStroke = "rgba(151,187,205,1)",
---          data = [20, 4, 87, 32, 46, 55, 38]
---        }
---       ]
---     }
+data2 = {
+      labels = ["January","February","March","April","May","June","July"],
+      datasets = [
+       {
+         fillColor = "rgba(220,220,220,0.5)",
+         strokeColor = "rgba(220,220,220,0.8)",
+         highlightFill = "rgba(220,220,220,0.75)",
+         highlightStroke = "rgba(220,220,220,1)",
+         data = [30, 34, 67, 12, 96, 75, 39]
+       },
+       {
+         fillColor = "rgba(151,187,205,0.5)",
+         strokeColor = "rgba(151,187,205,0.8)",
+         highlightFill = "rgba(151,187,205,0.75)",
+         highlightStroke = "rgba(151,187,205,1)",
+         data = [20, 4, 87, 32, 46, 55, 38]
+       }
+      ]
+    }
+data3 : Chart.LineChartData
+data3 = {
+      labels = ["January","February","March","April","May","June","July"],
+      datasets = [
+       {
+         fillColor = "rgba(220,220,220,0.5)",
+         strokeColor = "rgba(220,220,220,0.8)",
+         highlightFill = "rgba(220,220,220,0.75)",
+         highlightStroke = "rgba(220,220,220,1)",
+         data = [30, 34, 67, 12, 96, 75, 39],
+         mLabel = Nothing
+       },
+       {
+         fillColor = "rgba(151,187,205,0.5)",
+         strokeColor = "rgba(151,187,205,0.8)",
+         highlightFill = "rgba(151,187,205,0.75)",
+         highlightStroke = "rgba(151,187,205,1)",
+         data = [20, 4, 87, 32, 46, 55, 38],
+         mLabel = Just "xlabel"
+       }
+      ]
+    }
 
 main =
-    let b = Debug.log "data" data
-        c = Chart.radar chrt b options
+    let b = Debug.log "data" data3
+        c = Chart.line chrt b options
 --         c = Chart.bar chrt b options
     in plainText <| "hoge"
--- src/Chart.elm
+++ src/Chart.elm
@@ -1,30 +1,24 @@
-module Chart ( chart, line, bar, radar, polarArea, pie, doughnut ) where
-
-{-| Library for working with Charts. Email the mailing list if you encounter
-issues with internationalization or locale formatting.
-
-# Conversions
-@docs fromString, toTime, fromTime
-
-# Extractions
-@docs year, month, Month, day, dayOfWeek, Day, hour, minute, second, millisecond
-
--}
+module Chart ( chart, line, bar, radar, polarArea, pie, doughnut, LineChartData ) where
 
 import Native.Chart
+import Json.Encode (..)
+import List as L
+import List ((::))
+import Debug
+
 type Chart = Chart
 
 chart : String -> Chart
 chart = Native.Chart.chart
 
-line : Chart -> String -> String -> Chart
-line = Native.Chart.line
+line : Chart -> LineChartData -> String -> Chart
+line chart data options = Native.Chart.line chart (encodeLineChartData data) options
 
-bar : Chart -> String -> String -> Chart
-bar = Native.Chart.bar
+bar : Chart -> LineChartData -> String -> Chart
+bar chart data options = Native.Chart.bar chart (encodeLineChartData data) options
 
-radar : Chart -> String -> String -> Chart
-radar = Native.Chart.radar
+radar : Chart -> LineChartData -> String -> Chart
+radar chart data options = Native.Chart.radar chart (encodeLineChartData data) options
 
 polarArea : Chart -> String -> String -> Chart
 polarArea = Native.Chart.polarArea
@@ -34,3 +28,42 @@ pie = Native.Chart.pie
 
 doughnut : Chart -> String -> String -> Chart
 doughnut = Native.Chart.doughnut
+
+
+encodeLabels : List String -> Value
+encodeLabels = list << L.map string
+
+encodeLineChartData : LineChartData -> String
+encodeLineChartData { labels, datasets }
+    = encode 0 <| object [
+         ("labels", encodeLabels labels)
+       , ("datasets", list <| L.map encodeLineChartDataset datasets)
+      ]
+
+encodeLineChartDataset : LineChartDataset -> Value
+encodeLineChartDataset { fillColor, strokeColor, highlightFill, highlightStroke, data, mLabel }
+    = let ds : List (String, Value)
+          ds = [
+            ("fillColor", string fillColor)
+          , ("strokeColor", string strokeColor)
+          , ("highlightFill", string highlightFill)
+          , ("highlightStroke", string highlightStroke)
+          , ("data", list <| L.map int data)
+          ]
+      in case mLabel of
+           Just label -> object <| ("label", string label) :: ds
+           Nothing -> object ds
+
+type alias LineChartData = {
+      labels : List String
+    , datasets : List LineChartDataset
+    }
+
+type alias LineChartDataset = {
+      fillColor : String
+    , strokeColor : String
+    , highlightFill : String
+    , highlightStroke : String
+    , data : List Int
+    , mLabel : Maybe String
+    }
