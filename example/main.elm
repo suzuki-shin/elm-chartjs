22993c0cb36a76e88999986d22eb678c425c604c
Shinichiro Suzuki <shinichiro.su@gmail.com>
2015-04-21 17:42:27 +0900

    edit example.

--- example/main.elm
+++ example/main.elm
@@ -2,37 +2,8 @@ import Chart as C exposing (..)
 import Graphics.Element exposing (show)
 import Json.Encode exposing (..)
 
-options2 = { bezierCurve = False , barShowStroke = True }
-
-data2 : C.DataType2
-data2 = [ {
-          value = 300
-        , color = "#F7464A"
-        , highlight = "#FF5A5E"
-        , label = "Red"
-        },
-          {
-            value = 50
-          , color = "#46BFBD"
-          , highlight = "#5AD3D1"
-          , label = "Green"
-          },
-          {
-            value = 150
-          , color = "#6BFBD0"
-          , highlight = "#AD3D10"
-          , label = "hgoe"
-          },
-          {
-            value = 35
-          , color = "#BFBD00"
-          , highlight = "#D3D100"
-          , label = "fuga"
-          }
-        ]
-
-data3 : C.DataType1
-data3 = {
+data1 : C.DataType1
+data1 = {
       labels = ["January","February","March","April","May","June","July"],
       datasets = [
        {
@@ -62,8 +33,38 @@ data3 = {
       ]
     }
 
+data2 : C.DataType2
+data2 = [ {
+          value = 300
+        , color = "#F7464A"
+        , highlight = "#FF5A5E"
+        , label = "Red"
+        },
+          {
+            value = 50
+          , color = "#46BFBD"
+          , highlight = "#5AD3D1"
+          , label = "Green"
+          },
+          {
+            value = 150
+          , color = "#6BFBD0"
+          , highlight = "#AD3D10"
+          , label = "hgoe"
+          },
+          {
+            value = 35
+          , color = "#BFBD00"
+          , highlight = "#D3D100"
+          , label = "fuga"
+          }
+        ]
+
 main =
-    let
---        d = C.polarArea (C.chart "canvas") data2 options2
-       d = C.line (C.chart "canvas") options2 data3
-    in show <| "hoge"
+    let c1 = C.line (C.chart "lineChart") { bezierCurve = True } data1
+        c2 = C.bar (C.chart "barChart") {} data1
+        c4 = C.radar (C.chart "radarChart") {} data1
+        c3 = C.polarArea (C.chart "polarAreaChart") {} data2
+        c5 = C.pie (C.chart "pieChart") {} data2
+        c6 = C.doughnut (C.chart "doughnutChart") {} data2
+    in show ""
