c68b32e9685b1bcdd94679f8cf2c331ec193bea5
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2015-04-24 16:24:30 +0900

    Update main.elm

--- example/main.elm
+++ example/main.elm
@@ -1,6 +1,8 @@
 import Chart as C exposing (..)
 import Graphics.Element exposing (show)
 import Json.Encode exposing (..)
+import Signal exposing ((<~), sampleOn)
+import Mouse
 
 data1 : C.DataType1
 data1 = {
@@ -60,12 +62,42 @@ data2 = [ {
           }
         ]
 
+data1y x = {
+      labels = ["January","February","March","April","May","June","July"],
+      datasets = [
+       {
+         fillColor = "rgba(220,220,220,0.5)",
+         strokeColor = "rgba(220,220,220,0.8)",
+         highlightFill = "rgba(220,220,220,0.75)",
+         highlightStroke = "rgba(220,220,220,1)",
+         data = [30, 34, 67, 12, 96, 75, x],
+         mLabel = Nothing
+       },
+       {
+         fillColor = "rgba(151,187,205,0.5)",
+         strokeColor = "rgba(151,187,205,0.8)",
+         highlightFill = "rgba(151,187,205,0.75)",
+         highlightStroke = "rgba(151,187,205,1)",
+         data = [20, 4, 87, 32, 46, 55, 38],
+         mLabel = Just "xlabel"
+       },
+       {
+         fillColor = "rgba(251,87,205,0.5)",
+         strokeColor = "rgba(251,87,205,0.8)",
+         highlightFill = "rgba(251,87,205,0.75)",
+         highlightStroke = "rgba(251,87,205,1)",
+         data = [42, 84, 7, 73, 26, 5, 8],
+         mLabel = Just "xlabel"
+       }
+      ]
+    }
+
 main =
-    let c1 = C.line (C.chart "lineChart") { bezierCurve = True } data1
+    let
         c2 = C.bar (C.chart "barChart") {} data1
         c4 = C.radar (C.chart "radarChart") {} data1
         c3 = C.polarArea (C.chart "polarAreaChart") {} data2
         c5 = C.pie (C.chart "pieChart") {} data2
         c6 = C.doughnut (C.chart "doughnutChart") {} data2
-        c7 = addDataType1 c1 [100, 90] "Aug"
-    in show ""
+        c7 = addDataType1 c2 [100, 90] "Aug"
+    in (\x -> C.line (C.chart "lineChart") { bezierCurve = True } (data1y x) |> update |> \_ -> show x) <~ sampleOn Mouse.isDown Mouse.x
